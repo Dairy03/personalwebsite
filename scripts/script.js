b9769e4cd9e91aad63d9532e7d88a10f3c4c7d6a
Darien <dlowe39@my.bcit.ca>
2023-03-08 15:51:38 -0800

    implements check to see if entered prompt is a four character long number. also loops throught the course list to see if entered prompt matches the code in one of the courses, if it matches, console log Im in that class, and if it does not match, create a new object and add that object with the user entered code into it.

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,27 @@
-const courseList = {
-    item1: {code:'code', name:'name'},
-    item2: {code:'code', name:'name'},
-    item3: {code:'code', name:'name'}
+const courseList = [
+    {code:'ACIT 1620', name:'Fundemental Web Technologies'},
+    {code:'MATH 1310', name:'Technical Math for IT'},
+    {code:'COMM 1116', name:'Business Communications'}
+]
+
+let crscd
+do{
+    crscd = prompt('Please input a 4-digit number');
+} while(isNaN(crscd) || crscd.length !== 4)
+
+let check = 0
+
+for(let course of courseList){
+    if(course['code'].includes(crscd)){
+        console.log(`Yes I am taking the course: ${course['code']} - ${course['name']}`);
+        check = 1
+    }
+}
+    
+if(check == 0){
+    let newCourse = {code:'code', name:'name'}
+    newCourse["code"] = crscd;
+    newCourse['name'] = null;
+    courseList.push(newCourse)
+    console.log(courseList)
 }
